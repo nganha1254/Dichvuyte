b09ce3012982654b00aa54520faf129fa421467b
thanhtrucphan745 <phthanhtruc74nd@gmail.com>
2025-12-07 11:12:29 +0700

    xóa bản sql cũ kh nhầm

--- Models/DoanContext.cs
+++ Models/DoanContext.cs
@@ -55,7 +55,7 @@ public partial class DoanContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=ADMIN-PC; initial catalog=Doan; integrated security=True; TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("data source=LAPTOP-35P3J2BS\\MAY1; initial catalog=Doan; integrated security=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
